f630c62a7f5de2bf7397fb5e4301b2e7ecdb2b5e
andrewtodd <andrewtodd3@gmail.com>
2021-01-18 02:49:39 -0600

    updating tax years to go to 2030

--- app/controllers/taxes_controller.rb
+++ app/controllers/taxes_controller.rb
@@ -8,7 +8,7 @@ class TaxesController < ApplicationController
   	Rails.logger.debug("***************tax years: #{years}")
   	
   	if years['year'] == 'ALL' || years.nil? || years.empty?
-  		@tax_years = (2014..2019)
+  		@tax_years = (2014..2030)
   	else
   		@tax_years.push(years['year'].to_i)
   	end
